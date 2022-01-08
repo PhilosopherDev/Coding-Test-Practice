6d937364c6324e0d80ecf986b91a21ba64072007
PhilosopherProgrammer <nekarakube@gmail.com>
2022-01-08 22:44:14 +0900

    initial commit moved from Output Training

---
+++ CodingTestPlatform/LeetCode/1_Easy/1/1_TwoSum.js
@@ -0,0 +1,28 @@
+/**
+ * 1. Two Sum
+ * Easy
+ * https://leetcode.com/problems/two-sum/
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ var twoSum = function(nums, target) {
+    var answer = [];
+    // O(n)
+    var hashTable = nums.reduce((obj, num, idx) => {
+        obj[num] = idx;
+        return obj;
+    }, {});
+    
+    // O(n)
+    for(let i=0; i < nums.length; i++) {
+        // O(1)
+        if (hashTable[target - nums[i]] !== undefined && hashTable[target - nums[i]] !== i) {
+            answer.push(i, hashTable[target - nums[i]]);
+            break;
+        }
+    }
+    // O(n + n*1) => O(2n) => O(n)
+    return answer;
+};
+
