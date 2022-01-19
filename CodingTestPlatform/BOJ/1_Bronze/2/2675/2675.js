0714475d25397cc774cbb933900de6c98a857f4c
PhilosopherProgrammer <nekarakube@gmail.com>
2022-01-19 12:56:00 +0900

    2675 javascript solution

---
+++ CodingTestPlatform/BOJ/1_Bronze/2/2675/2675.js
@@ -0,0 +1,23 @@
+/**
+ * 문자열 반복
+ * https://www.acmicpc.net/problem/2675
+ */
+
+const [T, ...input] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const repeatStr = (str, num) => {
+    return str.split("").map((char) => {
+        let newChar = '';
+        for (let i = 0; i < num; i++) {
+            newChar += char;
+        }
+        return newChar;
+    }).join("");
+}
+
+const result = input.map((line) => {
+    const [R, str] = line.split(" ");
+    return repeatStr(str, R);
+}).join("\n");
+
+console.log(result);
