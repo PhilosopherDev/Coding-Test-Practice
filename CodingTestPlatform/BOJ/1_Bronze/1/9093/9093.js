6c79a45e01488cb3a42f6ca996f6762a69ebbf8c
PhilosopherProgrammer <nekarakube@gmail.com>
2022-01-19 12:31:07 +0900

    9093 solved by javascript

---
+++ CodingTestPlatform/BOJ/1_Bronze/1/9093/9093.js
@@ -0,0 +1,14 @@
+/**
+ * 단어 뒤집기
+ * https://www.acmicpc.net/problem/9093
+ */
+
+const [T, ...input] = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n");
+
+const result = input.map((str) => {
+    return str.split(" ").map((str2) => {
+        return str2.split("").reverse().join("");
+    }).join(" ");
+}).join("\n");
+
+console.log(result);
