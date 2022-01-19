013362f9fc18fce3204ef4e36c023ee4ec510b79
PhilosopherProgrammer <nekarakube@gmail.com>
2022-01-19 17:46:39 +0900

    1302 javascript solution

---
+++ CodingTestPlatform/BOJ/2_Silver/4/1302/1302.js
@@ -0,0 +1,18 @@
+/**
+ * 베스트셀러
+ * https://www.acmicpc.net/problem/1302
+ */
+
+const [N, ...books] = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n");
+
+const hashMap = {};
+let max = 0;
+
+books.forEach((book) => {
+    !hashMap[book] ? hashMap[book] = 1 : hashMap[book]++;
+    max = Math.max(max, hashMap[book]);
+});
+
+const result = books.filter((book) => hashMap[book] === max).sort()[0];
+
+console.log(result);
