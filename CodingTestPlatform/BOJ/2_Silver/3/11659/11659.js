26fd491a90b7b6402faef4f3414fddc37743d61a
PhilosopherProgrammer <nekarakube@gmail.com>
2022-01-20 17:59:03 +0900

    change let to const

--- CodingTestPlatform/BOJ/2_Silver/3/11659/11659.js
+++ CodingTestPlatform/BOJ/2_Silver/3/11659/11659.js
@@ -7,7 +7,7 @@
 const [NM, number, ...input] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const numArr = number.split(" ").map((c) => parseInt(c));
-let accSum = [0];
+const accSum = [0];
 
 for (let i = 0; i < numArr.length; i++) {
     accSum.push(accSum[i] + numArr[i]);
