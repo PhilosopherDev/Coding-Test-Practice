378300c24c0e53d3df5f36ab61bbccd4e9865d5c
PhilosopherProgrammer <nekarakube@gmail.com>
2022-02-11 17:22:50 +0900

    Change variable name

--- CodingTestPlatform/Programmers/Level1/신고결과받기/신고결과받기.js
+++ CodingTestPlatform/Programmers/Level1/신고결과받기/신고결과받기.js
@@ -5,21 +5,21 @@
 
 function solution(id_list, report, k) {
     const id_report = id_list.reduce((obj, id) => {
-        obj[id] = { setList: new Set([]), reported: 0 }
+        obj[id] = { reportList: new Set([]), reported: 0 }
         return obj;
     }, {});
 
     report.forEach((value) => {
         const [userId, reportedId] = value.split(" ");
-        if (!id_report[userId]["setList"].has(reportedId)) {
-            id_report[userId]["setList"].add(reportedId);
+        if (!id_report[userId]["reportList"].has(reportedId)) {
+            id_report[userId]["reportList"].add(reportedId);
             id_report[reportedId]["reported"]++;
         }
     });
 
     return id_list.map((id) => {
         let num = 0;
-        id_report[id].setList.forEach((l) => {
+        id_report[id].reportList.forEach((l) => {
             if (id_report[l].reported >= k) num++;
         });
         return num;
