67b4fc78e3adc9ce29d58f11bc5e5877933c6923
PhilosopherProgrammer <nekarakube@gmail.com>
2022-02-16 22:15:27 +0900

    object version

--- CodingTestPlatform/Programmers/Level2/방금그곡/방금그곡.js
+++ CodingTestPlatform/Programmers/Level2/방금그곡/방금그곡.js
@@ -21,6 +21,24 @@ function solution(m, musicinfos) {
     return resultArr[0][2];
 }
 
+function solution2(m, musicinfos) {
+    const resultArr = musicinfos.map((info) => {
+        const [start, end, name, scale] = info.split(",");
+        const time = calcTime(start, end);
+        const scaleArr = replaceStr(scale).split("");
+
+        const sRepeat = Math.floor(time / (scaleArr.length));
+        const sRemainder = time % (scaleArr.length);
+        const repeatedScale = scaleArr.join("").repeat(sRepeat) + scaleArr.slice(0, sRemainder).join("")
+
+        return { time, repeatedScale, name };
+    }).filter((item) => item.repeatedScale.indexOf(replaceStr(m)) > -1).sort((a, b) => b.time - a.time);
+    
+    if (!resultArr.length) return "(None)";
+    
+    return resultArr[0].name;
+}
+
 function calcTime(start, end) {
     const [sHour, sMinute] = start.split(":").map((item) => parseInt(item));
     const [eHour, eMinute] = end.split(":").map((item) => parseInt(item));
@@ -32,4 +50,4 @@ function replaceStr(str) {
     return str.replace(/C#/g, "c").replace(/D#/g, "d").replace(/F#/g, "f").replace(/G#/g, "g").replace(/A#/g, "a");
 }
 
-module.exports = solution;
+module.exports = solution2;
