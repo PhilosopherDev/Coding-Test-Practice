8c370de17d3e8d9806188ddb2ee2e59c2f7bfe0b
PhilosopherProgrammer <nekarakube@gmail.com>
2022-02-18 11:33:58 +0900

    주식가격 python

---
+++ CodingTestPlatform/Programmers/Level2/주식가격/주식가격.py
@@ -0,0 +1,20 @@
+# 주식가격.py
+# https://programmers.co.kr/learn/courses/30/lessons/42584
+
+from collections import deque
+def solution(prices):
+    answer = []
+    prices = deque(prices)
+    while prices:
+        c = prices.popleft()
+
+        count = 0
+        for i in prices:
+            if c > i:
+                count += 1
+                break
+            count += 1
+
+        answer.append(count)
+
+    return answer
