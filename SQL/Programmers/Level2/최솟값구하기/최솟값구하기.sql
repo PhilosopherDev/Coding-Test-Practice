1bb321a37377176ebbc6f83b4f687c5e078a6ba0
PhilosopherProgrammer <nekarakube@gmail.com>
2022-03-11 11:44:20 +0900

    최솟값 구하기 - MIN

---
+++ SQL/Programmers/Level2/최솟값구하기/최솟값구하기.sql
@@ -0,0 +1,8 @@
+/*
+    최솟값구하기.sql
+    https://programmers.co.kr/learn/courses/30/lessons/59415?language=mysql
+*/
+
+SELECT DATETIME FROM ANIMAL_INS ORDER BY DATETIME ASC LIMIT 1
+
+SELECT MIX(DATETIME) FROM ANIMAL_INS
