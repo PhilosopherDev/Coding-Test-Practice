45bceb8a682a642903308f9ed9a5bf292efd5c88
PhilosopherProgrammer <nekarakube@gmail.com>
2022-04-10 23:28:18 +0900

    직사각형 별찍기 - String

---
+++ CodingTestPlatform/Programmers/Level1/직사각형별찍기/직사각형별찍기.js
@@ -0,0 +1,17 @@
+/**
+ * 직사각형별찍기.js
+ * https://programmers.co.kr/learn/courses/30/lessons/12969?language=javascript
+ */
+
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', data => {
+    const n = data.split(" ");
+    const a = Number(n[0]), b = Number(n[1]);
+    for (let i = 0; i < b; i++) {
+        let star = '';
+        for (let j = 0; j < a; j++) {
+            star += '*';
+        }
+        console.log(star);
+    }
+});
